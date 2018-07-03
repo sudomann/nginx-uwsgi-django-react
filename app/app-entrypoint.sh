9a56e8ad1b36aff27fe73a29a03b6418f5df3d5b
sudomann <njundongw@yahoo.com>
2018-07-03 15:34:51 -0400

    Updated prod configuration

--- app/app-entrypoint.sh
+++ app/app-entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 # start uwsgi server using django's production_settings
-env DJANGO_SETTINGS_MODULE=sampledjangoproject.production_settings uwsgi --master --ini $APP_SERVICE_ROOT_DIR/uwsgi.ini
+uwsgi --master --ini $APP_SERVICE_ROOT_DIR/uwsgi.ini
